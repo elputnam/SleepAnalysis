2b3f7f36a70a0b19ec40e7c12045fd68acdea9f0
fleshcircuit <elputnam@inaction.ie>
2021-08-20 17:43:33 +0100

    updated and added more elements

--- sketch.js
+++ sketch.js
@@ -1,48 +1,93 @@
 let L = 0;
 let secs = 0;
-// let list1 = [];
+let list1 = [];
 let sleep = [];
+let rad = 0;
 
 function preload(){
   //Load list of json file names
-  // list1 = loadStrings('dataList.txt');
-  sleep = loadJSON("data/sleep-2020-05-30.json");
+  list1 = loadStrings('dataList.txt');
+  // sleep = loadJSON("data/sleep-2020-05-30.json");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100, 100);
-  data = sleep[L]["levels"]["data"];
-  frameRate(1);
+  // data = sleep[L]["levels"]["data"];
+  frameRate(5);
+  
   //select month
-  // let month = int(random(1,15));
-  // sleep = loadJSON(list1[month]);
+  let month = int(random(1,15));
+  print(month);
+  sleep = loadJSON(list1[month]);
   
 }
 
 function draw() {
-  //background(220);
-  
-  // if (frameCount >= 10){
-    // data = sleep[L]["levels"]["data"];
+  //circles
+  let x = rad;
+  let y = 0;
+  push();
+  translate(width/2, height/2);
+  let num = 30;
+  let cir = (360 / num) * (frameCount % num);
+  rotate((radians(cir)));
+  for (let i = 0; i < height*.03; i++){
+    noFill();
+    stroke(random(255));
+    circle(x, y, random(200));
+  }
+  rad += 1;
+  pop();
+
+  //initial background
+  if (frameCount == 1){
+    background(10);
+  }
+
+  //sleep mapping
+  if (frameCount >= 20){
+    data = sleep[L]["levels"]["data"];
+
     // sleep level mapping
     let sleepLevel = data[frameCount % data.length]["level"];
     let duration = data[frameCount % data.length]["seconds"];
-    print(sleepLevel, duration);
-
-    if (sleepLevel == ["wake"]){
-     frameCount = 0;
-     while(frameCount > duration);
-        background(0, 100, 100, 10);
+    
+    let dur = log(duration);
+    let sat = map(duration, 0, 20, 1, 100);
+    // let sat = log(timeWarp);
+    let alp = 10;
+    print(sleepLevel, duration, dur);
+    // squares
+    for ( let i = 0; i < width*.5; i++){
+      fill(random(360), random(100), random(100));
+      square(random(width), random(height), random(5,10));
     }
+
+
+    // changing backgrounds
+    if (sleepLevel == ["wake"]){
+        background(0, sat, 100, alp);
+    }
+    
     if (sleepLevel == ["deep"]){
-      background(100, 100, 100, 10);
+      background(100, sat, 100, alp);
     }
     if (sleepLevel == ["light"]){
-      background(200, 100, 100, 10);
+      background(200, sat, 100, alp);
     }
     if (sleepLevel == ["rem"]){
-      background(300, 100, 100, 10);
+      background(300, sat, 100, alp);
+    }
+    if (sleepLevel == ["restless"]){
+      background(10, sat);
+    }
+    if (sleepLevel == ["asleep"]){
+      background( sat);
     }
   }
-// }
+
+  if (x == width){
+    rad = 0;
+  }
+}
