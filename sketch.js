69722e2fce81e1f1800ea3e852e320eae63bd7b9
elputnam <elputnam@inaction.ie>
2021-08-29 11:07:56 +0100

    Changed design; fixed looping

--- sketch.js
+++ sketch.js
@@ -14,7 +14,6 @@ let night_data_index; // index for data points for a night
 var sat; //saturation of background
 var alp; //alpha of background
 
-
 //text
 let title = ['sleep', 'between', 'disrupted', 'dreams']
 let dreamText = []; //poem
@@ -22,15 +21,6 @@ var xT;
 var yT;
 let j = 0;
 let t = 5;
-
-//crawling ivy
-var maxCount; // max count of the squares
-var currentCount = 1;
-var xIvy = [];
-var y = [];
-var r = [];
-var x2 = [];
-var y2 = [];
 
 //images
 var awakeFace;
@@ -54,24 +44,21 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100, 100);
-  frameRate(15);
+  frameRate(10);
   maxCount = height*.25;
-  
 
   //text set up
   xT = width*.5;
   yT = height*.7;
   
   //select month
-  let month = int(random(1,14));
+  let month = int(random(14));
   print(month);
   sleep = loadJSON(list1[month]);
   num_nights = Object.keys(sleep).length;
   
-  //ivy first square
-  xIvy[0] = width*.25;
-  y[0] = height / 2;
-  r[0] = 50;
+  //grid
+  tileCount = height/30;
 }
 
 function draw() {
@@ -81,7 +68,7 @@ function draw() {
  
   }
   if (frameCount < 450){  
-    background(10, 5);
+    background(10, 10);
     circles();
     if (frameCount%15==0 ){
       textFont('Titillium Web')
@@ -104,31 +91,33 @@ function draw() {
       new_night = false;
       night_data_index = 0;
   }
+  if (frameCount%2 == 0){  
     sleepMapping();
-    // nightCounter();
-    // dreamMode();
-    snow();
+    grid();
+    }
   
   if (frameCount >= 450){
     if (frameCount%10==0){
-    
     textSize(45);
     noStroke();
-    fill(0);
     textAlign(LEFT);
     xT = random(width*.5);
     yT = random(height * .8, height);
+    fill(200)
+    rect(xT-10, yT-50, width, height* .2)
+    fill(0);
     text(dreamText[t], xT, yT);
+    
     t += 1;
     }
 
-    if (t == 35){
+    if (t == 28){
       t = int(random(27));
     }
 
-    if (frameCount%3==0){
-      pixelGrowth();
-      }
+    // if (frameCount%3==0){
+    //   pixelGrowth();
+    //   }
    }
 }
 
@@ -147,68 +136,39 @@ function circles(){
   rotate((radians(cir)));
   for (let i = 0; i < height*.08; i++){
     noFill();
+    strokeWeight(random(3));
     stroke(random(250, 300), random(100), random(100));
+    // fill(random(255), 5)
     circle(random(x), random(y), 5*i);
   }
   pop();
 }
 
-function snow(){
-  for ( let i = 0; i < width*.25; i++){
-    noStroke();
-    fill(random(255));
-    rect(random(width/2), random(height), random(5,10), random(5,10));
-  }
-}
+function grid() {
+  for (let gridY = 0; gridY < tileCount; gridY++) {
+    for (let gridX = 0; gridX < tileCount; gridX++) {
+      let posX = (width*.5 / tileCount) * gridX;
+      let posY = (height / tileCount) * gridY;
 
-function pixelGrowth(){
-  // create a random set of parameters
-  var newR = random(1, 7);
-  var newX = random(newR, width - newR);
-  var newY = random(newR, height - newR);
+      //introduce random choice between two
+      let toggle = int(random(0, 3));
 
-  var closestDist = Number.MAX_VALUE;
-  var closestIndex = 0;
-  // which circle is the closest?
-  for (var i = 0; i < currentCount; i++) {
-    var newDist = dist(newX, newY, xIvy[i], y[i]);
-    if (newDist < closestDist) {
-      closestDist = newDist;
-      closestIndex = i;
+      if (toggle == 0) {
+        stroke(random(255));
+      
+        line(posX, posY, posX + width/tileCount, posY + height
+        );
+      }
+      
+      if (toggle == 1) {
+        fill(random(255));
+        square(
+          random(posX, posX + width / tileCount),
+          random(posY, posY + height / tileCount),
+          random(tileCount)
+        );
+      }
     }
-  }
-  
-  // align it to the closest circle outline
-  var angle = atan2(newY - y[closestIndex], newX - xIvy[closestIndex]);
-
-  x2[currentCount] = newX;
-  y2[currentCount] = newY;
-  xIvy[currentCount] = xIvy[closestIndex] + cos(angle) * (r[closestIndex] + newR);
-  y[currentCount] = y[closestIndex] + sin(angle) * (r[closestIndex] + newR);
-  r[currentCount] = newR;
-  currentCount++;
-
-
-  for (var i = 0; i < currentCount; i++) {
-    if (i == 0) {
-      // xIvy[0] = mouseX
-      // y[0] = mouseY
-      noStroke()
-      // fill(random(360), 100, random(100));
-      fill(random(255));
-    } else {
-      fill(random(360), 100, random(100));
-    }
-    rectMode(CENTER)
-    rect(xIvy[i], y[i], r[i] * 2, r[i] * 2);
-  }
-
-  if (currentCount >= maxCount){
-    currentCount = 1;
-  xIvy[0] = random(width*.25);
-  y[0] = random(height);
-  r[0] = random(50,100);
-    //maxCount = random(100,1000);
   }
 }
 
@@ -244,10 +204,22 @@ if (sleepLevel == ["rem"]){
   image(remFace, width/2, 0, width/2, height);
   }
 
+if (sleepLevel == ["restless"]){
+  background(0);
+  // tint(100, sat, 100, alp);
+  image(remFace, width/2, 0, width/2, height);
+}
+
+if (sleepLevel == ["asleep"]){
+  background(255);
+  // tint(200, sat, 100, alp);
+  image(deepFace, width/2, 0, width/2, height);
+}
+
   //Counter loop
-  // text(dateTime, 40, 40);
-  // text("Night: " + night_index + " Reading: " + night_data_index, 40, 80);
-  // text(sleepLevel + duration, 40, 120);
+  text(dateTime, 40, 40);
+  text("Night: " + night_index + " Reading: " + night_data_index, 40, 80);
+  text(sleepLevel + duration, 40, 120);
 
   night_data_index += 1;
   if (night_data_index === night_data_length) {
@@ -255,8 +227,8 @@ if (sleepLevel == ["rem"]){
   new_night = true;
   night_index += 1;
   // are there any more nights?
-  if (night_index === num_nights) {
-  night_index = 0;
+  if (night_index === 32) {
+    night_index = 0;
         }
       }
   }
